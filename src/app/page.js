d0e33201215192cb2e3ba8e6f277861510ba81ef
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-03-22 11:38:03 +0530

    responsive

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Mainsection from "@/components/Mainsection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
       <Mainsection />
     </main>
   );
